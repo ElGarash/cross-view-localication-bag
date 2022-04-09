9648ff3d046228ff4b617856e30ebf8fde7ae697
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2022-04-10 01:53:15 +0200

    Revert to `check_call`

--- generate_descriptors.py
+++ generate_descriptors.py
@@ -1,29 +1,28 @@
+#!/usr/bin/env python3
 import subprocess
 
 
-SCRIPTS_ROOT_PATH = "/kaggle/cross-view-localization-bag/"
+SCRIPTS_ROOT_PATH = "/kaggle/cross-view-localization-bag"
 
 
 def generate_dsm_descriptors():
     print("Generating DSM descriptors...")
-    return subprocess.Popen(
-        ["python", f"{SCRIPTS_ROOT_PATH}/DSM/script/generate_descriptors.py"],
-        shell=True,
+    return subprocess.check_call(
+        ["python", f"{SCRIPTS_ROOT_PATH}/DSM/script/generate_descriptors.py"]
     )
 
 
 def generate_siam_fca_net_descriptor():
     print("Generating Siam-FCANet descriptors...")
-    return subprocess.Popen(
-        ["python", f"{SCRIPTS_ROOT_PATH}/Siam-FCANet/generate_sat_descriptors.py"],
-        shell=True,
+    return subprocess.check_call(
+        ["python", f"{SCRIPTS_ROOT_PATH}/Siam-FCANet/generate_sat_descriptors.py"]
     )
 
 
 def generate_l2ltr_descriptor():
     print("Generating L2LTR descriptors...")
-    return subprocess.Popen(
-        ["python", f"{SCRIPTS_ROOT_PATH}/L2LTR/generate_sat_descriptors.py"], shell=True
+    return subprocess.check_call(
+        ["python", f"{SCRIPTS_ROOT_PATH}/L2LTR/generate_sat_descriptors.py"]
     )
 
 
@@ -31,9 +30,6 @@ def generate_descriptors():
     p1 = generate_dsm_descriptors()
     p2 = generate_siam_fca_net_descriptor()
     p3 = generate_l2ltr_descriptor()
-    
-    for p in (p1, p2, p3):
-        p.wait() 
 
 
 if __name__ == "__main__":
